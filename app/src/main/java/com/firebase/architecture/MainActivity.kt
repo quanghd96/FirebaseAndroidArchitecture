ce1ac4ffd8e4713afefe871f759351784c7e5156
Đặng Ngọc Quảng <quanghd96@gmail.com>
2019-05-21 23:20:01 +0700

    add databinding

--- app/src/main/java/com/firebase/architecture/MainActivity.kt
+++ app/src/main/java/com/firebase/architecture/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.firebase.architecture.databinding.ActivityMainBinding
@@ -16,20 +17,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listMobile)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        lvMobile.adapter = adapter
-//        binding.adapter = adapter
-//        adapter.notifyDataSetChanged()
+        binding.adapter = adapter
 
         val mobileViewModel = ViewModelProviders.of(this).get(MobileViewModel::class.java)
         mobileViewModel.getAllMobile().observe(this, Observer {
             adapter.clear()
             adapter.addAll(it)
             adapter.notifyDataSetChanged()
-//            Toast.makeText(this, it.size.toString(), Toast.LENGTH_LONG).show()
         })
     }
 }
