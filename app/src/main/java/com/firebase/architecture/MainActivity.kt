c2f43fb2dd541972397e7b499b8611a3d5d15ec8
Quang Dang <quanghd96@gmail.com>
2019-06-13 15:11:04 +0700

    add mobile dialog

--- app/src/main/java/com/firebase/architecture/MainActivity.kt
+++ app/src/main/java/com/firebase/architecture/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.firebase.architecture
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.firebase.architecture.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.my_dialog.*
+import kotlinx.android.synthetic.main.my_dialog.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mobileViewModel: MobileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         )
         binding.adapter = adapter
 
-        val mobileViewModel = ViewModelProviders
+        mobileViewModel = ViewModelProviders
             .of(this)
             .get(MobileViewModel::class.java)
         mobileViewModel.getAllMobile().observe(this, Observer {
@@ -34,7 +40,22 @@ class MainActivity : AppCompatActivity() {
         })
 
         btnAdd.setOnClickListener {
-            mobileViewModel.addMobile("")
+            showAddMobileDialog()
         }
+    }
+
+    private fun showAddMobileDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.my_dialog, viewGroup, false)
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        builder.setTitle("Add mobile")
+        builder.setPositiveButton("OK") { _, _ ->
+            mobileViewModel.addMobile(dialogView.edtName.text.toString())
+        }
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
